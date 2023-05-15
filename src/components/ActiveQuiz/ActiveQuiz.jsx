b2e5bac2ba0e17a468798432ddb2d5be6bb35586
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-05-15 16:02:04 +0300

    Implementation change question

--- src/components/ActiveQuiz/ActiveQuiz.jsx
+++ src/components/ActiveQuiz/ActiveQuiz.jsx
@@ -1,13 +1,21 @@
 import AnswersList from '../AnswersList/AnswersList';
 import styles from './ActiveQuiz.module.css';
-export default function ActiveQuiz({ answers, question, onAnswerClick }) {
+export default function ActiveQuiz({
+  answers,
+  question,
+  onAnswerClick,
+  quizLength,
+  answerNumber,
+}) {
   return (
     <div className={styles.activeQuiz}>
       <p className={styles.question}>
         <span>
-          <strong>2.</strong>&nbsp; {question}
+          <strong>{answerNumber}.</strong>&nbsp; {question}
         </span>
-        <small>2 из 10</small>
+        <small>
+          {answerNumber} из {quizLength}
+        </small>
       </p>
       <AnswersList answers={answers} onAnswerClick={onAnswerClick} />
     </div>
--- src/containers/Quiz/Quiz.jsx
+++ src/containers/Quiz/Quiz.jsx
@@ -6,10 +6,58 @@ export default class Quiz extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      activeQuestion: 0,
       quiz: [
         {
+          id: 1,
           rightAnswerId: 2,
+          question: 'Вопрос номер 1?',
+          answers: [
+            {
+              text: 'Ответ 1',
+              id: 1,
+            },
+            {
+              text: 'Ответ 2',
+              id: 2,
+            },
+            {
+              text: 'Ответ 3',
+              id: 3,
+            },
+            {
+              text: 'Ответ 4',
+              id: 4,
+            },
+          ],
+        },
+        {
+          id: 3,
+          rightAnswerId: 1,
           question: 'Вопрос номер 2?',
+          answers: [
+            {
+              text: 'Ответ 1',
+              id: 1,
+            },
+            {
+              text: 'Ответ 2',
+              id: 2,
+            },
+            {
+              text: 'Ответ 3',
+              id: 3,
+            },
+            {
+              text: 'Ответ 4',
+              id: 4,
+            },
+          ],
+        },
+        {
+          id: 2,
+          rightAnswerId: 4,
+          question: 'Вопрос номер 3?',
           answers: [
             {
               text: 'Ответ 1',
@@ -35,6 +83,7 @@ export default class Quiz extends Component {
 
   onAnswerClick = (answerId) => {
     console.log(answerId);
+    this.setState({ activeQuestion: this.state.activeQuestion + 1 });
   };
 
   render() {
@@ -43,9 +92,11 @@ export default class Quiz extends Component {
         <div className={styles.quiz__wrapper}>
           <h1>Ответьте на все вопросы</h1>
           <ActiveQuiz
-            answers={this.state.quiz[0].answers}
-            question={this.state.quiz[0].question}
+            answers={this.state.quiz[this.state.activeQuestion].answers}
+            question={this.state.quiz[this.state.activeQuestion].question}
             onAnswerClick={this.onAnswerClick}
+            quizLength={this.state.quiz.length}
+            answerNumber={this.state.activeQuestion + 1}
           />
         </div>
       </div>
