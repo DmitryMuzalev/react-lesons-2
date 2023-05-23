81f3839011b9fea672cc71ece4142b239f2b2cd0
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-05-23 11:58:46 +0300

    Login form styling

--- src/containers/AuthPage/AuthPage.jsx
+++ src/containers/AuthPage/AuthPage.jsx
@@ -1,9 +1,25 @@
-//import styles from './AuthPage.module.css';
+import styles from './AuthPage.module.css';
 
 export default function AuthPage() {
   return (
     <div className="container">
-      <h2>Авторизация</h2>
+      <div className={styles.auth}>
+        <h2>Авторизация</h2>
+        <form className={styles.authForm}>
+          <label htmlFor="">
+            Логин :
+            <input type="email" autoFocus />
+          </label>
+          <label htmlFor="">
+            Пароль :
+            <input type="password" />
+          </label>
+          <div className={styles.authForm__btns}>
+            <button type="submit">Войти</button>
+            <button>Зарегистрироваться</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
